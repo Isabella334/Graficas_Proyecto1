35595cb3659db10bd346784aae96ebee8b443ba5
Isabella334 <isabellarecinos@gmail.com>
2025-11-21 00:02:12 -0600

    refactor: Game object

---
+++ src/game.rs
@@ -0,0 +1,86 @@
+use crate::framebuffer::Framebuffer;
+use crate::maze::load_maze;
+use crate::player::Player;
+use crate::renderer::*;
+use crate::textures::TextureManager;
+use raylib::math::Vector2;
+use raylib::prelude::*;
+use std::f32::consts::PI;
+
+pub enum GameState {
+    Playing,
+}
+
+pub struct Game {
+    window: RaylibHandle,
+    raylib_thread: RaylibThread,
+    framebuffer: Framebuffer,
+    player: Player,
+    texture_manager: TextureManager,
+    block_size: usize,
+    game_state: GameState,
+}
+
+impl Game {
+    pub fn new(window_width: i32, window_height: i32, block_size: usize) -> Self {
+        let (mut window, raylib_thread) = raylib::init()
+            .size(window_width, window_height)
+            .title("Raycaster - Isa Recinos")
+            .log_level(TraceLogLevel::LOG_WARNING)
+            .build();
+
+        window.set_target_fps(60);
+
+        let framebuffer = Framebuffer::new(window_width as u32, window_height as u32);
+
+        let player = Player {
+            pos: Vector2 {
+                x: 150.0,
+                y: 150.0,
+            },
+            a: PI / 3.0,
+            fov: PI / 3.0,
+            lives: 3
+        };
+
+        let texture_manager = TextureManager::new(&mut window, &raylib_thread);
+
+        Self {
+            window,
+            raylib_thread,
+            framebuffer,
+            player,
+            texture_manager,
+            block_size,
+            game_state: GameState::Playing,
+        }
+    }
+
+    pub fn run(&mut self) {
+        self.framebuffer.set_background_color(Color::BLUE);
+
+        while !self.window.window_should_close() {
+            self.framebuffer.clear();
+
+            match self.game_state {
+                GameState::Playing => {
+                    let maze = load_maze("maze.txt");
+
+                    let mut mode = "3D";
+                    if self.window.is_key_down(KeyboardKey::KEY_M) {
+                        mode = if mode == "2D" { "3D" } else { "2D" };
+                    }
+
+                    if mode == "2D" {
+                        render_maze(&mut self.framebuffer, &maze, self.block_size, &self.player);
+                    } else {
+                        render_3d(&mut self.framebuffer, &maze, &self.player, self.block_size, &self.texture_manager);
+                    }
+
+                    self.framebuffer.swap_buffers(&mut self.window, &self.raylib_thread);
+                }
+            }
+        }
+    }
+}
+
---
+++ src/renderer.rs
@@ -0,0 +1,155 @@
+use crate::framebuffer::Framebuffer;
+use crate::maze::Maze;
+use crate::player::Player;
+use crate::caster::cast_ray;
+use crate::sprites::Sprite;
+use crate::textures::TextureManager;
+use raylib::prelude::*;
+use std::f32::consts::PI;
+
+pub fn draw_sprite(
+    framebuffer: &mut Framebuffer,
+    maze: &Maze,
+    player: &Player,
+    sprite: &Sprite,
+    texture_manager: &TextureManager,
+) {
+    let screen_width = framebuffer.width as f32;
+    let screen_height = framebuffer.height as f32;
+
+    let sprite_a = (sprite.pos.y - player.pos.y).atan2(sprite.pos.x - player.pos.x);
+    let mut angle_difference = sprite_a - player.a;
+    while angle_difference > PI {
+        angle_difference -= 2.0 * PI
+    }
+    while angle_difference < -PI {
+        angle_difference += 2.0 * PI
+    }
+    if angle_difference.abs() > player.fov / 2.9 {
+        return;
+    }
+
+    let sprite_d = ((player.pos.x - sprite.pos.x).powi(2) + (player.pos.y - sprite.pos.y).powi(2)).sqrt();
+    if sprite_d < 50.0 || sprite_d > 1000.0 {
+        return;
+    }
+
+    let ray = cast_ray(framebuffer, maze, player, sprite_a, 100, false);
+    let behind_wall = sprite_d >= ray.distance;
+    if behind_wall {
+        return;
+    }
+
+    let sprite_size = (screen_height / sprite_d) * 40.0;
+    let screen_x = ((angle_difference / player.fov) + 0.5) * screen_width;
+
+    let start_x = (screen_x - sprite_size / 2.0).max(0.0) as usize;
+    let start_y = (screen_height / 2.0 - sprite_size / 2.0).max(0.0) as usize;
+
+    let end_x = (start_x + sprite_size as usize).min(framebuffer.width as usize);
+    let end_y = (start_y + sprite_size as usize).min(framebuffer.height as usize);
+
+    for x in start_x..end_x {
+        for y in start_y..end_y {
+            let tx = sprite.start_anim_x + ((x - start_x) as u32 * sprite.frame_width / sprite_size as u32);
+            let ty = sprite.start_anim_y + ((y - start_y) as u32 * sprite.frame_height / sprite_size as u32);
+
+            let color = texture_manager.get_pixel_color(sprite.texture_key, tx, ty);
+            if color.a > 0 {
+                framebuffer.set_current_color(color);
+                framebuffer.set_pixel(x as u32, y as u32);
+            }
+        }
+    }
+}
+
+fn draw_cell(framebuffer: &mut Framebuffer, x0: usize, y0: usize, block_size: usize, cell: char) {
+    if cell == ' ' {
+        return;
+    }
+    framebuffer.set_current_color(Color::DARKOLIVEGREEN);
+
+    for x in x0..x0 + block_size {
+        for y in y0..y0 + block_size {
+            framebuffer.set_pixel(x as u32, y as u32);
+        }
+    }
+}
+
+fn cell_to_texture_color(texture_cache: &TextureManager, cell: char, tx: u32, ty: u32) -> Color {
+    texture_cache.get_pixel_color(cell, tx, ty)
+}
+
+pub fn render_maze(
+    framebuffer: &mut Framebuffer,
+    maze: &Maze,
+    block_size: usize,
+    player: &Player,
+) {
+    for (row_index, row) in maze.iter().enumerate() {
+        for (col_index, &cell) in row.iter().enumerate() {
+            let x0 = col_index * block_size;
+            let y0 = row_index * block_size;
+            draw_cell(framebuffer, x0, y0, block_size, cell);
+        }
+    }
+
+    framebuffer.set_current_color(Color::RED);
+    let px = player.pos.x as u32;
+    let py = player.pos.y as u32;
+    framebuffer.set_pixel(px, py);
+
+    let num_rays = 5;
+    for i in 0..num_rays {
+        let current_ray = i as f32 / num_rays as f32;
+        let a = player.a - (player.fov / 2.0) + (player.fov * current_ray);
+        cast_ray(framebuffer, maze, player, a, block_size, true);
+    }
+}
+
+pub fn render_3d(
+    framebuffer: &mut Framebuffer,
+    maze: &Maze,
+    player: &Player,
+    block_size: usize,
+    texture_manager: &TextureManager,
+) {
+    let num_rays = framebuffer.width;
+    let hh = framebuffer.height as f32 / 2.0;
+
+    for y in 0..hh as usize {
+        framebuffer.set_current_color(Color::SKYBLUE);
+        for x in 0..framebuffer.width {
+            framebuffer.set_pixel(x as u32, y as u32);
+        }
+    }
+
+    for y in (hh as usize)..framebuffer.height as usize {
+        framebuffer.set_current_color(Color::DARKGREEN);
+        for x in 0..framebuffer.width {
+            framebuffer.set_pixel(x as u32, y as u32);
+        }
+    }
+
+
+    for i in 0..num_rays {
+        let current_ray = i as f32 / num_rays as f32;
+        let a = player.a - (player.fov / 2.0) + (player.fov * current_ray);
+        let intersect = cast_ray(framebuffer, &maze, &player, a, block_size, false);
+
+        let distance_to_wall = intersect.distance;
+        let distance_to_projection_plane = 70.0;
+        let stake_height = (hh / distance_to_wall) * distance_to_projection_plane;
+
+        let stake_top = (hh - (stake_height / 2.0)).max(0.0) as usize;
+        let stake_bottom = (hh + (stake_height / 2.0)).min(framebuffer.height as f32) as usize;
+
+        for y in stake_top..stake_bottom {
+            let ty = (y as f32 - stake_top as f32) / (stake_bottom as f32 - stake_top as f32) * 128.0;
+
+            let color = cell_to_texture_color(texture_manager, intersect.impact, intersect.tx as u32, ty as u32);
+            framebuffer.set_current_color(color);
+            framebuffer.set_pixel(i, y as u32);
+        }
+    }
+}
